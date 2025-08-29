c271157208fba938b0abaed72e866db7f2be7263
MaheshBabriwal25635426 <157110938+MaheshBabriwal25635426@users.noreply.github.com>
2025-08-29 07:28:24 +0000

    datatypes

---
+++ one_basic/02_datatype.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) // we are using node js not a web browser
+
+//console.log(3+3);
+//console.log(3+3) // both are ture // code readability my be good 
+
+
+////number => 2 to poewr 53
+//bigint 
+//string =>""
+
+//boolean => t/f
+//null => standalone value 
+//underfined => 
+    //symbol => unique
+
+//object
+
+let name = "mahi"
+let age = 18
+let isLoggodIn = false 
+
+
+console.log(typeof "name");
+console.log(typeof age);
+console.log(typeof isLoggodIn)
+
