48ee61bc0513408f209b7c5682c1a9cc8ac93db1
MaheshBabriwal25635426 <157110938+MaheshBabriwal25635426@users.noreply.github.com>
2025-09-02 08:12:49 +0000

    datatypes_summary.js

---
+++ one_basic/datatypes_summary.js
@@ -0,0 +1,32 @@
+//primitive
+
+//7 types :- String , Number ,Boolearn, Null, undefined , Symbol , BigInt 
+
+
+const score = 100
+const scoreVAlue =100.3
+
+const isLoggedId = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherID = Symbol('123')
+console.log(id === anotherID);
+
+
+// Referance type :- (NON Primitive)
+//Array, Objects ,Functions 
+
+const heros = ["virat ", "dhoni"]
+let myObj ={
+    name: "Mahesh",
+    age: 22,
+}
+
+const myfunction = function(){
+    console.log("Hello");
+    
+}
+
+// console.log(typeof bignumber);
