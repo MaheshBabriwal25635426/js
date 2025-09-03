72205ca6108b76d2836247bda6095ed4ae0d18f6
MaheshBabriwal25635426 <157110938+MaheshBabriwal25635426@users.noreply.github.com>
2025-09-03 06:07:50 +0000

    06_String.js

---
+++ one_basic/06_String.js
@@ -0,0 +1,247 @@
+const name ="Mahesh"
+const repoCount = 50
+//console.log(name + repoCount + " Value");
+
+//use backtikes
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('mahiiiii')
+
+
+
+// google console page pr dikh raha he 
+// const gamename = new String('mahii')
+// undefined
+// gamename
+// String {'mahii'}0: "m"1: "a"2: "h"3: "i"4: "i"length: 5[[Prototype]]: String[[PrimitiveValue]]: "mahii"
+
+console.log(gameName[0])
+
+//function and methods
+
+console.log(gameName.length);
+
+console.log(gameName.toUpperCase());
+
+
+console.log(gameName.charAt(2));
+
+const newStringOne = "    hitesh   "
+console.log(newStringOne);
+console.log(newStringOne.trim);
+
+
+const url = "https://mahi.com/mahi%21hi"
+console.log(url.replace('%21','_'));
+
+/*
+this is all about string
+String {'mahii'}
+0
+: 
+"m"
+1
+: 
+"a"
+2
+: 
+"h"
+3
+: 
+"i"
+4
+: 
+"i"
+length
+: 
+5
+[[Prototype]]
+: 
+String
+anchor
+: 
+ƒ anchor()
+at
+: 
+ƒ at()
+big
+: 
+ƒ big()
+blink
+: 
+ƒ blink()
+bold
+: 
+ƒ bold()
+charAt
+: 
+ƒ charAt()
+charCodeAt
+: 
+ƒ charCodeAt()
+codePointAt
+: 
+ƒ codePointAt()
+concat
+: 
+ƒ concat()
+constructor
+: 
+ƒ String()
+endsWith
+: 
+ƒ endsWith()
+fixed
+: 
+ƒ fixed()
+fontcolor
+: 
+ƒ fontcolor()
+fontsize
+: 
+ƒ fontsize()
+includes
+: 
+ƒ includes()
+indexOf
+: 
+ƒ indexOf()
+isWellFormed
+: 
+ƒ isWellFormed()
+italics
+: 
+ƒ italics()
+lastIndexOf
+: 
+ƒ lastIndexOf()
+length
+: 
+0
+link
+: 
+ƒ link()
+localeCompare
+: 
+ƒ localeCompare()
+match
+: 
+ƒ match()
+matchAll
+: 
+ƒ matchAll()
+normalize
+: 
+ƒ normalize()
+length
+: 
+0
+name
+: 
+"normalize"
+arguments
+: 
+(...)
+caller
+: 
+(...)
+[[Prototype]]
+: 
+ƒ ()
+[[Scopes]]
+: 
+Scopes[0]
+padEnd
+: 
+ƒ padEnd()
+padStart
+: 
+ƒ padStart()
+repeat
+: 
+ƒ repeat()
+replace
+: 
+ƒ replace()
+replaceAll
+: 
+ƒ replaceAll()
+search
+: 
+ƒ search()
+slice
+: 
+ƒ slice()
+small
+: 
+ƒ small()
+split
+: 
+ƒ split()
+startsWith
+: 
+ƒ startsWith()
+strike
+: 
+ƒ strike()
+sub
+: 
+ƒ sub()
+substr
+: 
+ƒ substr()
+substring
+: 
+ƒ substring()
+sup
+: 
+ƒ sup()
+toLocaleLowerCase
+: 
+ƒ toLocaleLowerCase()
+toLocaleUpperCase
+: 
+ƒ toLocaleUpperCase()
+toLowerCase
+: 
+ƒ toLowerCase()
+toString
+: 
+ƒ toString()
+toUpperCase
+: 
+ƒ toUpperCase()
+toWellFormed
+: 
+ƒ toWellFormed()
+trim
+: 
+ƒ trim()
+trimEnd
+: 
+ƒ trimEnd()
+trimLeft
+: 
+ƒ trimStart()
+trimRight
+: 
+ƒ trimEnd()
+trimStart
+: 
+ƒ trimStart()
+valueOf
+: 
+ƒ valueOf()
+Symbol(Symbol.iterator)
+: 
+ƒ [Symbol.iterator]()
+[[Prototype]]
+: 
+Object
+[[PrimitiveValue]]
+: 
+""
+[[PrimitiveValue]]
+: 
+"mahii"
+*/
