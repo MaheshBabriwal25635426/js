01b9844c5e4eea92d001c2605ce2ca539b485573
MaheshBabriwal25635426 <157110938+MaheshBabriwal25635426@users.noreply.github.com>
2025-09-05 09:10:35 +0000

    08_date.js

---
+++ one_basic/08_Date.js
@@ -0,0 +1,30 @@
+// go to the MDN for more about to dates
+//date
+
+let myDate = new Date()//instance 
+
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toUTCString());
+// console.log(myDate.getTimezoneOffset());
+
+ let myCreatedDAte =new Date (2025, 09, 08)
+// console.log(myCreatedDAte.toDateString()); //method
+
+
+//let myCreatedDAte =new Date (2025, 0, 8, 5)
+
+// let myCreatedDAtereatedDAte =new Date ("2025-01-12")
+// console.log(myCreatedDAte.toLocaleString());
+
+let myTimeStamp =Date.now()
+
+console.log(myTimeStamp);
+
+
+console.log(myCreatedDAte.getTime());
