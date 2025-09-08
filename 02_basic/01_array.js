0d62814434c39a289642284dca3ae67f8a5346db
MaheshBabriwal25635426 <157110938+MaheshBabriwal25635426@users.noreply.github.com>
2025-09-08 08:20:00 +0000

    01_array.js

--- 02_basic/01_array.js
+++ 02_basic/01_array.js
@@ -1,2 +1,56 @@
-let add=10;
-console.log(add);
+// let add=10;
+// console.log(add);
+// 
+
+// Array
+//go mdn for more info
+
+
+//const myArr = [10,20,30,40,50]
+
+//console.log(myArry[0]);
+
+
+// const myHeros= ["mahesh" ,"magarahhjdsbajw" ]
+
+
+//const myArry1 = new Array[1,2,3,4,5]
+
+//console.log(myArr[1]);
+//let a=10
+//console.log(a);
+
+// myArr.push(6)
+// myArr.push(7)
+// console.log(myArr);
+// myArr.pop()
+
+
+// myArr.unshift(0) //push in front of array
+// myArr.shift()//to delete front element
+
+
+//console.log(myArr.includes(6));
+//console.log(myArr.indexOf(2));
+
+
+
+
+// console.log(myArr);
+
+const myArr2 = new Array(0,1,2,3,4,5)
+// console.log(myArr2[1]);
+
+// slice ,splice
+
+console.log("A" ,myArr2);
+
+const myn1 = myArr2.slice(1,3)
+
+console.log(myn1);
+console.log("B", myArr2);
+
+const myn2 = myArr2.splice(1,3)
+console.log("c", myArr2);
+
+console.log(myn2);
