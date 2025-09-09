6b83e89b5fef7cab7902488c166dab6a8efe994f
MaheshBabriwal25635426 <157110938+MaheshBabriwal25635426@users.noreply.github.com>
2025-09-09 06:54:26 +0000

    03_object.js

---
+++ 02_basic/03_object.js
@@ -0,0 +1,36 @@
+//singleton 
+
+//object literals
+
+const mySym = Symbol("key1")
+
+
+const jsuser ={
+    name: "Mahi", //name automatic string me convert kr dega 
+    age: 18,
+    location: "Indore",
+    email: "mahi@gmail.com",
+    isLoggedIn: false,
+    lastloginDays: ["Monday", "Sunday"],
+    "Full name": "Mahi babriwal",
+    //mySym:"mykey1", //object
+    [mySym]:"mykey1" //symbol key 
+
+}
+console.log(jsuser.email);
+console.log(jsuser["email"]);
+console.log(jsuser["Full name"]); // mdn datatypes for more information
+
+//console.log(jsuser.mySym);
+
+//console.log(typeof jsuser.mySym); //give string but we want Symbol than ,
+
+console.log(jsuser[mySym]);
+
+
+jsuser.email ="mahesh@gmail.com"
+Object.freeze(jsuser)// do not chsange after this frizee
+
+jsuser.email ="gjcj@gmail.com"
+
+console.log(jsuser);
