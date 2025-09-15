55463d3e65e50f2389e081a2022ece338f8b06dc
MaheshBabriwal25635426 <157110938+MaheshBabriwal25635426@users.noreply.github.com>
2025-09-15 14:26:12 +0530

    01_Function(basic)

---
+++ 03_basic/01_function.js
@@ -0,0 +1,45 @@
+// console.log("m");
+// console.log("a");
+// console.log("h");
+// console.log("e");
+// console.log("s");
+// console.log("h");
+
+
+
+//keyword  name 
+function sayMyname(){
+    console.log("m");
+console.log("a");
+console.log("h");
+console.log("e");
+console.log("s");
+console.log("h");
+}
+
+
+//refrence : sayMyname
+//sayMyname() 
+// commment ke bad print is liye nhi ki vo abhi function me he if vo function ke bahar hota to vo print ho jata 
+
+
+// function addtwonum(num1,num2)
+// {
+//     console.log(num1+num2);
+    
+// }
+//print
+//addtwonum(2,3) //output 5
+//addtwonum(2,"3")  //output 23
+//addtwonum(2,"a") //output 2a
+
+function addTwoNumbers(number1, number2){
+
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+
+const result = addTwoNumbers(3, 5)
+
+// console.log("Result: ", result);
